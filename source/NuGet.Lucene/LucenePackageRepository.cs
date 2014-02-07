71f8af88cd5b4b1a99f46a3da4a6f3dfe337492f
Chris Eldredge <chris.eldredge@gmail.com>
2014-02-07 02:44:55 +0000

    Use real path, not computed path, when loading package and metadata from filesystem.

--- source/NuGet.Lucene/LucenePackageRepository.cs
+++ source/NuGet.Lucene/LucenePackageRepository.cs
@@ -318,8 +318,7 @@ namespace NuGet.Lucene
         public LucenePackage LoadFromFileSystem(string path)
         {
             var fullPath = FileSystem.GetFullPath(path);
-            var package = Convert(OpenPackage(fullPath), new LucenePackage(_ => FileSystem.OpenFile(path)));
-            package.Path = FileSystem.MakeRelative(path);
+            var package = Convert(OpenPackage(fullPath), new LucenePackage(FileSystem) { Path = FileSystem.MakeRelative(fullPath) });
             return package;
         }
 
@@ -342,10 +341,12 @@ namespace NuGet.Lucene
         {
             CopyPackageData(package, lucenePackage);
 
-            var path = GetPackageFilePath(lucenePackage);
-            lucenePackage.Path = path;
+            if (string.IsNullOrWhiteSpace(lucenePackage.Path))
+            {
+                lucenePackage.Path = GetPackageFilePath(lucenePackage);    
+            }
 
-            CalculateDerivedData(package, lucenePackage, path, () => lucenePackage.GetStream());
+            CalculateDerivedData(package, lucenePackage, lucenePackage.Path, lucenePackage.GetStream);
 
             return lucenePackage;
         }
